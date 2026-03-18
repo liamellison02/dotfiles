fb5ed610d5e0ba18c3db17d0f1be1bc9cf549612
liamellison-ns <liam.ellison@nscorp.com>
2026-03-18 09:34:49 -0400

    add oh-my-zsh configs + update .zshrc

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,80 @@
+################################
+#####                      #####
+#####     core helpers     #####
+#####                      #####
+################################
+
+alias ali="nvim ~/.oh-my-zsh/custom/aliases.zsh"
+alias aliases="nvim ~/.oh-my-zsh/custom/aliases.zsh"
+alias vsh="nvim ~/.zshrc"
+alias src="source ~/.zshrc"
+
+
+################################
+#####                      #####
+#####       dir nav        #####
+#####                      #####
+################################
+
+alias cdg="cd ~/git/"
+alias xx="cd ~/scratch/ && nvim"
+alias ml="cd ~/git/mlcpp/"
+alias dsa="cd ~/git/dsa-solutions/"
+
+
+################################
+#####                      #####
+#####     don't            #####
+#####     abbreviate       #####
+#####     competitive      #####
+#####     programming      #####
+#####                      #####
+################################
+
+alias dsa="cd ~/git/dsa-solutions/"
+alias lc='sed -n 30,63p ~/git/dsa-solutions/templates/lc.cpp | pbcopy'
+alias cpp="clang++ -std=c++20 -O2"
+
+
+################################
+#####                      #####
+#####        git           #####
+#####        push          #####
+#####        git           #####
+#####        paid          #####
+#####                      #####
+################################
+
+alias gcm="git commit -m"
+alias ga="git add"
+alias gp="git push"
+alias gpf="git push --force"
+alias gpl="git pull"
+alias gsc="git switch -c"
+alias gsw="git switch"
+alias gdh="git diff HEAD"
+alias gs="git status"
+alias gb="git branch"
+alias grs="git restore --staged"
+alias gca="git commit --amend --no-edit"
+
+
+################################
+#####                      #####
+#####       kubectl        #####
+#####                      #####
+################################
+
+alias k="kubectl"
+alias kl="kubectl logs"
+alias kgj="kubectl get jobs"
+alias kgjn="kubectl get jobs -n"
+alias kgp="kubectl get pods"
+alias kgpn="kubectl get pods -n"
+alias kdj="kubectl describe jobs"
+alias kdjn="kubectl describe jobs -n"
+alias kdp="kubectl describe pods"
+alias kdpn="kubectl describe pods -n"
+
+
+
